0d81f37a27456ac4e913ad283866dee75cf33d5e
koziscool <kozjob@yahoo.com>
2016-03-25 12:55:03 -0500

    toggled completed

--- todo.js
+++ todo.js
@@ -6,22 +6,58 @@ todo.controller( 'TodoCtrl', [
     '$scope',
     function( $scope) {
 
-      $scope.items = [ {text: "sample item", dueDate: new Date(), completed: false}, {text: "completed item", dueDate: new Date(), completed: true} ];
+      $scope.items = [ 
+        {text: "sample item", dueDate: new Date(), completed: false}, 
+        {text: "completed item", dueDate: new Date(), completed: true} 
+      ];
+
       $scope.text;
       $scope.dueDate;
+      $scope.showCompleted = true;
 
       $scope.newItem = function(text, dueDate) {
         var item = { text: text,
                         dueDate: dueDate,
                         completed: false }
         $scope.items.push(item);
+        console.log( $scope.items )
         $scope.text = "";
         $scope.dueDate = "";
       };
 
-    $scope.show = function( thing ) {
-      return thing;
+    $scope.clearCompleted = function(  ) {
+      $scope.items = $scope.items.filter( function(item) {
+        return !item.completed;
+      });
     }
+
+    $scope.toggleCompleted = function(  ) {
+      $scope.showCompleted = !$scope.showCompleted;
+    }
+
+
+    $scope.deleteItem = function(index) {
+      $scope.items.splice(index, 1);
+    };
 
   }
 ]);
+
+todo.filter('filterByShowCompleted', function(  ){
+  return function( items, showBool ) {
+
+    if( showBool ) { 
+      return items; 
+    }
+
+    var retItems = [];
+    angular.forEach( items, function( item ){
+      // If the kitten is playful, keep it!
+      if( item.completed === false ){
+        retItems.push( item ); 
+      }
+    });
+    return retItems;
+  }
+
+});
