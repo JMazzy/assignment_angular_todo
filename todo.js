ec08b7ae029680c1c505077bd979d83953cd9486
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-25 12:42:28 -0400

    checkboxes

--- todo.js
+++ todo.js
@@ -2,13 +2,22 @@
 
 var todo = angular.module('todoApp', []);
 
-todo.controller( 'TodoCtrl', [ 
+todo.controller( 'TodoCtrl', [
     '$scope',
-    function( $scope) { 
+    function( $scope) {
 
-    $scope.item = { text: "Get groceries from the store",
-                    dueDate: new Date( 2016, 8, 7),
-                    completed: false }
+      $scope.items = [ {text: "sample item", dueDate: new Date(), completed: false}, {text: "completed item", dueDate: new Date(), completed: true} ];
+      $scope.text;
+      $scope.dueDate;
+
+      $scope.newItem = function(text, dueDate) {
+        var item = { text: text,
+                        dueDate: dueDate,
+                        completed: false }
+        $scope.items.push(item);
+        $scope.text = "";
+        $scope.dueDate = "";
+      };
 
     $scope.show = function( thing ) {
       return thing;
@@ -16,6 +25,3 @@ todo.controller( 'TodoCtrl', [
 
   }
 ]);
-
-
-
